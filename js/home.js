2a42a109aaef392c87be1c2102dc725cdf6b4a79
hsuaung <hsuaung633@gmail.com>
2024-01-19 14:02:41 +0630

    second

--- js/home.js
+++ js/home.js
@@ -25,7 +25,7 @@ function showSlides(n) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
+    // dots[slideIndex - 1].className += " active";
 } 
 
 
@@ -38,3 +38,24 @@ function showSlides(n) {
     }
     document.getElementById(menuItem).style.display = "block";
 }
+
+//hamburger menu
+let menu = document.querySelector("#menu-icon");
+let navlist = document.querySelector(".nav-menu");
+let icon = menu.querySelector(".fa-bars");
+menu.onclick = () => {
+    // menu.classList.toggle('x');
+    console.log("hello");
+    if (icon.classList.contains("fa-bars")) {
+        icon.classList.replace("fa-bars", "fa-square-xmark");
+    }
+    else {
+        icon.classList.replace("fa-square-xmark","fa-bars");
+    }
+     navlist.classList.toggle('open');
+};
+window.onscroll = () => {
+    menu.classList.remove('open');
+    navlist.classList.remove('open');
+};
+
